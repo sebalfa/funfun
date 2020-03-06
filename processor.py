6753175366dbf0b19c40b687b9725466df296a1f
DanielKSNielsen <dan94nielsen@gmail.com>
2020-03-06 16:29:16 +0100

    First Pull

--- processor.py
+++ processor.py
@@ -1,1 +1,10 @@
-print("Hello World")
+import grabber
+import common_funcs as cf
+import lxml.html
+
+branch = grabber.Branch('https://ekstrabladet.dk/opinionen/madskastrup/saa-piskede-mette-en-stemning-op/8035340')
+branch.pull()
+
+content = branch.contents
+
+cf.uprint(content)
