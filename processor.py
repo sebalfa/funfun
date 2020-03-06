caf1573009ff5286f8b46847e81b1fd98984d406
DanielKSNielsen <dan94nielsen@gmail.com>
2020-03-06 16:29:36 +0100

    First Pull

--- processor.py
+++ processor.py
@@ -1,6 +1,5 @@
 import grabber
 import common_funcs as cf
-import lxml.html
 
 branch = grabber.Branch('https://ekstrabladet.dk/opinionen/madskastrup/saa-piskede-mette-en-stemning-op/8035340')
 branch.pull()
