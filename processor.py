0548c5ef03c46e058221aa6837bbcc3ede237b6b
DanielKSNielsen <dan94nielsen@gmail.com>
2020-03-07 09:58:00 +0100

    Creating function

--- processor.py
+++ processor.py
@@ -1,9 +1,12 @@
 import grabber
 import common_funcs as cf
 
-branch = grabber.Branch('https://ekstrabladet.dk/opinionen/madskastrup/saa-piskede-mette-en-stemning-op/8035340')
-branch.pull()
+def my_function(x):
+    branch = grabber.Branch(x)
+    branch.pull()
 
-content = branch.contents
+    content = branch.contents
 
-cf.uprint(content)
+    cf.uprint(content)
+
+my_function('https://ekstrabladet.dk/ferie/ikonisk-stenstatue-paa-paaskeoeen-er-oedelagt/8034294')
